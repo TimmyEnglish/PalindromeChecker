943129485374fbd5f2cfa8247560ca28ab5792e7
Tymofii Pilipchuk <timapilimail@gmail.com>
2024-08-27 09:48:00 +0300

    Add project files.

---
+++ PalindromeChecker/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿using System.Windows.Forms;
+
+namespace PalindromeChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputTextBox.ForeColor = System.Drawing.Color.Silver;
+            this.inputTextBox.Location = new System.Drawing.Point(250, 131);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(277, 35);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.Text = "Input word";
+            this.inputTextBox.Enter += new System.EventHandler(this.inputTextBox_Enter);
+            this.inputTextBox.Leave += new System.EventHandler(this.inputTextBox_Leave_1);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnCheck.CausesValidation = false;
+            this.btnCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCheck.Location = new System.Drawing.Point(318, 198);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(144, 52);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(208, 301);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 26);
+            this.resultLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "PalindromeChecker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button btnCheck;
+        private Label resultLabel;
+    }
+}
+
---
+++ PalindromeChecker/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PalindromeChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string inputWord = inputTextBox.Text;
+            if (string.IsNullOrWhiteSpace(inputWord))
+            {
+                resultLabel.Text = "Please enter a word to check.";
+                resultLabel.ForeColor = System.Drawing.Color.Red;
+                return;
+            }
+            PalindromeChecker checker = new PalindromeChecker(inputWord);
+
+            string cleanedWord = checker.CleanedWord;
+
+            if (checker.IsPalindrome())
+            {
+                resultLabel.Text = $"'{cleanedWord}' is a palindrome.";
+                resultLabel.ForeColor = System.Drawing.Color.LimeGreen;
+            }
+            else
+            {
+                resultLabel.Text = $"'{cleanedWord}' is not a palindrome.";
+                resultLabel.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        private void inputTextBox_Enter(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text == "Input word")
+            {
+                inputTextBox.Text = "";
+
+                inputTextBox.ForeColor = Color.Black;
+            }
+        }
+        private void inputTextBox_Leave_1(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text == "")
+            {
+                inputTextBox.Text = "Input word";
+
+                inputTextBox.ForeColor = Color.Silver;
+            }
+        }
+    }
+}
